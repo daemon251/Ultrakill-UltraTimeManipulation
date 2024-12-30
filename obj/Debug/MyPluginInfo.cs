a6f670034b03c1ecf7e6c142128ca1662c1cf41c
daemon <51888735+daemon251@users.noreply.github.com>
2024-12-29 18:21:05 -0600

    uploaded obj folder - forgot earlier

---
+++ obj/Debug/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FasterPunch
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "FasterPunch";
+        public const string PLUGIN_NAME = "";
+        public const string PLUGIN_VERSION = "";
+    }
+}
